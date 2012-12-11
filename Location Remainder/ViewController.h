4509b1efbab68ca1435005655dada1c3532a1490
Nayamode <Nayamode@Nayamodes-iMac.local>
2012-12-11 12:59:09 +0530

    Initial Commit

---
+++ Location Remainder/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Location Remainder
+//
+//  Created by Nayamode on 11/12/12.
+//  Copyright (c) 2012 Nayamode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
